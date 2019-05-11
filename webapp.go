29ea2235c4985e08e3b5929fd3a706bdea534c1d
Cate Miller <cate.marie.m@gmail.com>
2019-05-11 13:38:41 -0700

    add edit ability

--- webapp.go
+++ webapp.go
@@ -17,7 +17,7 @@ func (p *Entry) save() error {
     return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) (*Entry, error) {
+func loadEntry(title string) (*Entry, error) {
     filename := title + ".txt"
     body, err := ioutil.ReadFile(filename)
     if err != nil {
@@ -28,11 +28,30 @@ func loadPage(title string) (*Entry, error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
-    p, _ := loadPage(title)
+    p, _ := loadEntry(title)
     fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadEntry(title)
+    if err != nil {
+        p = &Entry{Title: title}
+    }
+    fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+        "<form action=\"/save/%s\" method=\"POST\">"+
+        "<textarea name=\"body\">%s</textarea><br>"+
+        "<input type=\"submit\" value=\"Save\">"+
+        "</form>",
+        p.Title, p.Title, p.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    http.HandleFunc("/save/", saveHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
